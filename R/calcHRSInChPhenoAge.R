2c253bb05570d4f8bc9baa2108cc36b18107c096
jkasa13 <54773013+jkasa13@users.noreply.github.com>
2024-12-07 16:42:47 -0800

    Update calcHRSInChPhenoAge.R

--- R/calcHRSInChPhenoAge.R
+++ R/calcHRSInChPhenoAge.R
@@ -10,8 +10,8 @@
 #' @return If you added the optional pheno input (preferred) the function appends a column with the clock calculation and returns the dataframe. Otherwise, it will return a vector of calculated clock values in order of the
 #' @export
 #'
-#' @examples calcHRSInChPhenoAge(exampleBetas, examplePheno, imputation = F)
-calcHRSInChPhenoAge <- function(DNAm, pheno = NULL, CpGImputation = NULL, imputation = F){
+#' @examples calcHRSInChPhenoAge(exampleBetas, examplePheno, imputation = T)
+calcHRSInChPhenoAge <- function(DNAm, pheno = NULL, CpGImputation = NULL, imputation = T){
 
   #######################
   ### Read in the Data###
